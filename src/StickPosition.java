432c97ab1203bb9d90b97e404aba515d93ad136e
bbb651 <bar.ye651@gmail.com>
2021-04-09 17:58:01 +0300

    Analog stick rework (WIP)

--- src/StickPosition.java
+++ src/StickPosition.java
@@ -3,111 +3,131 @@ import java.awt.event.MouseEvent;
 
 public class StickPosition {
 
-    private int x;
-    private int y;
+	private int x;
+	private int y;
 
-    private double theta;
-    private double radius;
+	private double theta;
+	private double radius;
 
-    private final static int MAX_SIZE = 32767;
+	private final static int MAX_SIZE = 32767;
 
-    private StickPosition previousPos;
-
-    public StickPosition (){
-        this(0,0);
-    }
-
-    public StickPosition (StickPosition pos){
-        this(pos.getX(), pos.getY());
-    }
-
-    public StickPosition (int x, int y){
+    public StickPosition(int x, int y) {
         this.x = x;
         this.y = y;
-    }
-
-    private void updatePolar (){
-        radius = Math.sqrt(Math.pow(x,2) + Math.pow(y,2))/(double)MAX_SIZE;
-
-        if (radius > 1){
-            radius = 1;
-            updateCart();
-        }
-
-        if (x >= 0 && y >= 0) {
-            theta = Math.atan((double) y / (double) x);
-        }
-
-        if ((x < 0 && y >= 0) || (x < 0 && y < 0)){
-            theta = Math.PI - Math.atan( (double) y/ (double) x);
-        }
-
-        if (x >= 0 && y < 0){
-            theta = (Math.PI*2) + Math.atan((double)y/(double)x);
-        }
-
-       // System.out.println("theta: " + Math.toDegrees(theta));
-
-    }
-
-    private void updateCart (){
-        x = (int)((radius*MAX_SIZE)*Math.cos(theta));
-        y = (int)((radius*MAX_SIZE)*Math.sin(theta));
-    }
-
-    public double getAngle (Point circleOrigin, MouseEvent e){
-        double side1 = e.getX() - circleOrigin.getX();
-        double side2 = e.getY() - circleOrigin.getY();
-
-        double hypot = Math.hypot(side1,side2);
-
-        double alpha = Math.asin(side1/hypot);
-        double beta = Math.asin(side2/hypot);
-
-        return alpha;
-    }
-
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public double getTheta() {
-        return theta;
-    }
-
-    public double getRadius() {
-        return radius;
-    }
-
-    public static int getMaxSize() {
-        return MAX_SIZE;
-    }
-
-    public void setXY(int x, int y) {
-
-        previousPos = new StickPosition(this.x,this.y);
-        this.x = x;
-        this.y = y;
-
         updatePolar();
     }
 
-    public void setBackPos (){
-        //setXY(previousPos.getX(), previousPos.getY());
-    }
+	public StickPosition() {
+		this(0, 0);
+	}
 
-    public void setTheta(double theta) {
-        this.theta = theta;
-        updateCart();
-    }
+    public StickPosition(StickPosition pos) {
+		x = pos.x;
+		y = pos.y;
+		theta = pos.theta;
+		radius = pos.radius;
+	}
 
-    public void setRadius(double radius) {
-        this.radius = radius;
-        updateCart();
-    }
+	private void updatePolar() {
+		radius = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) / (double) MAX_SIZE;
+
+		if (radius > 1) {
+			radius = 1;
+			updateCart();
+		}
+
+		if (x >= 0) {
+			theta = Math.atan((double) y / (double) x);
+		}
+
+		if (x < 0 && y >= 0) {
+			theta = Math.atan((double) y / (double) x) + Math.PI;
+		}
+
+		if (x < 0 && y < 0) {
+			theta = Math.atan((double) y / (double) x) - Math.PI;
+		}
+
+		if (x == 0 && y > 0) {
+			theta = Math.PI / 2;
+		}
+
+		if (x == 0 && y < 0) {
+			theta = -Math.PI / 2;
+		}
+
+		if (x == 0 && y == 0) {
+			theta = Double.NaN;
+		}
+	}
+
+	private void updateCart() {
+		x = (int) ((radius * MAX_SIZE) * Math.cos(theta));
+		y = (int) ((radius * MAX_SIZE) * Math.sin(theta));
+	}
+
+	public double getAngle (Point circleOrigin, MouseEvent e){
+		double side1 = e.getX() - circleOrigin.getX();
+		double side2 = e.getY() - circleOrigin.getY();
+
+		double hypot = Math.hypot(side1,side2);
+
+		double alpha = Math.asin(side1/hypot);
+		double beta = Math.asin(side2/hypot);
+
+		return alpha;
+	}
+
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public double getTheta() {
+		return theta;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+		updatePolar();
+	}
+
+	public void setY(int y) {
+		this.y = y;
+		updatePolar();
+	}
+
+	public void setPosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+		updatePolar();
+	}
+
+	public void setTheta(double theta) {
+		this.theta = theta;
+		updateCart();
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+		updateCart();
+	}
+
+	@Override
+	public String toString() {
+		return "StickPosition{" +
+				"x=" + x +
+				", y=" + y +
+				", theta=" + theta +
+				", radius=" + radius +
+				'}';
+	}
 }
