b9fb7ef97fa1d8106fa4ccf209a2e4ebc02d4521
MonsterDruide1 <5958456@gmail.com>
2021-04-27 13:59:13 +0200

    Fix layout of FunctionEditor

--- src/main/java/io/github/jadefalke2/Components/FunctionEditorWindow.java
+++ src/main/java/io/github/jadefalke2/Components/FunctionEditorWindow.java
@@ -1,8 +1,10 @@
 package io.github.jadefalke2.Components;
 
 import io.github.jadefalke2.Script;
+import sun.awt.image.GifImageDecoder;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class FunctionEditorWindow extends JFrame {
 
@@ -10,21 +12,22 @@ public class FunctionEditorWindow extends JFrame {
 	private JPanel mainPanel;
 
 	public FunctionEditorWindow (){
-		mainPanel = new JPanel();
+		mainPanel = new JPanel(new GridBagLayout());
 		pianoRoll = new PianoRoll(Script.getEmptyScript(5));
 	}
 
 	public void startUp (){
-		setVisible(true);
-		setSize(200,100);
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.BOTH;
+		c.weighty = 1;
+		c.weightx = 1;
 		setLocation(200,200);
 
-
-		mainPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
-
 		JScrollPane scrollPane = new JScrollPane(pianoRoll);
-		mainPanel.add(scrollPane);
+		mainPanel.add(scrollPane, c);
 
 		add(mainPanel);
+		pack();
+		setVisible(true);
 	}
 }
