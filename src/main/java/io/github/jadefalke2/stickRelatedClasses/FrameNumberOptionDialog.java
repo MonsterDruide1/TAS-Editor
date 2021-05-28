331a10a85f4c073845becbdf5018274fab454378
MonsterDruide1 <5958456@gmail.com>
2021-05-28 20:03:26 +0200

    Removed useless helper function of "keep stick position"

--- src/main/java/io/github/jadefalke2/stickRelatedClasses/FrameNumberOptionDialog.java
+++ src/main/java/io/github/jadefalke2/stickRelatedClasses/FrameNumberOptionDialog.java
@@ -7,24 +7,6 @@ import java.awt.*;
 
 public class FrameNumberOptionDialog {
 
-	public static int getFrameNumber (){
-
-		// creates the spinner
-		SpinnerNumberModel model = new SpinnerNumberModel(1, 0, 500, 1);
-		JSpinner spinner = new JSpinner(model);
-
-		// opens the dialog and saves the option into an int
-		UIManager.put("OptionPane.minimumSize",new Dimension(200,100));
-		int option = JOptionPane.showOptionDialog(null, spinner, "Enter number of frames", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, 1);
-
-		// if the option is ok it returns the value
-		if (option == JOptionPane.OK_OPTION) {
-			return (int)spinner.getValue();
-		}
-
-		return 0;
-	}
-
 	public static StickPosition[] getSmoothTransitionData (Settings settings) {
 		// creates the spinner
 		SpinnerNumberModel model = new SpinnerNumberModel(1, 0, 500, 1);
