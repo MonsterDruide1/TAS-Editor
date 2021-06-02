f0dc7d28efb5580933189e474b871ea08bed50c4
Jadefalke2 <79963380+Jadefalke2@users.noreply.github.com>
2021-06-02 17:53:06 +0200

    better format

--- src/main/java/io/github/jadefalke2/util/Logger.java
+++ src/main/java/io/github/jadefalke2/util/Logger.java
@@ -1,9 +1,17 @@
 package io.github.jadefalke2.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Logger {
 
 	public static void log (String s) {
 		// using this to make it easier to later change this behaviour
-		System.out.println(s);
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		String fullDateFormatted = "[" + dtf.format(now) + "]: ";
+
+		System.out.println(fullDateFormatted + s);
 	}
 }
