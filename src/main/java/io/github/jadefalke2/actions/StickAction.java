130850d9a8f36668bd2a442f5fbb49e211ae1f30
MonsterDruide1 <5958456@gmail.com>
2022-09-23 16:54:06 +0200

    StickAction: Array of new positions

--- src/main/java/io/github/jadefalke2/actions/StickAction.java
+++ src/main/java/io/github/jadefalke2/actions/StickAction.java
@@ -12,23 +12,26 @@ public class StickAction implements Action {
 	private final Script script;
 	private final int[] rows;
 	private final JoystickPanel.StickType stickType;
-	private final StickPosition newPosition;
+	private final StickPosition[] newPositions;
 	private final StickPosition[] oldPositions;
 
-	public StickAction(Script script, int[] rows, JoystickPanel.StickType stickType, StickPosition newPosition) {
+	public StickAction(Script script, int[] rows, JoystickPanel.StickType stickType, StickPosition[] newPositions) {
 		this.script = script;
 		this.rows = rows;
 		this.stickType = stickType;
-		this.newPosition = newPosition;
+		this.newPositions = newPositions;
 		oldPositions = Arrays.stream(rows).mapToObj(script::getLine)
 			.map(stickType == JoystickPanel.StickType.L_STICK ? InputLine::getStickL : InputLine::getStickR)
 			.toArray(StickPosition[]::new);
 	}
+	public StickAction(Script script, int[] rows, JoystickPanel.StickType stickType, StickPosition newPosition) {
+		this(script, rows, stickType, Arrays.stream(rows).mapToObj(i -> newPosition).toArray(StickPosition[]::new));
+	}
 
 	@Override
 	public void execute() {
-		for (int row : rows) {
-			script.setStickPos(row, stickType, newPosition);
+		for (int i=0; i<rows.length; i++) {
+			script.setStickPos(rows[i], stickType, newPositions[i]);
 		}
 	}
 
@@ -41,6 +44,6 @@ public class StickAction implements Action {
 
 	@Override
 	public String toString() {
-		return "Stick Action, at frames: " + Arrays.toString(rows) + "; " + stickType + "; new Stickpostion:" + newPosition;
+		return "Stick Action, at frames: " + Arrays.toString(rows) + "; " + stickType + "; new Stickpostions:" + Arrays.toString(newPositions);
 	}
 }
