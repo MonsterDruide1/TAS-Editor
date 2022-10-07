96339613ff34c72037a7ff5d25ed211e2cf54950
MonsterDruide1 <5958456@gmail.com>
2022-10-07 12:06:47 +0200

    CorruptedScriptException: Accept cause as param

--- src/main/java/io/github/jadefalke2/util/CorruptedScriptException.java
+++ src/main/java/io/github/jadefalke2/util/CorruptedScriptException.java
@@ -4,11 +4,14 @@ import javax.swing.JOptionPane;
 
 public class CorruptedScriptException extends Exception {
 
-	public CorruptedScriptException (String errorMessage, int frame){
-		super(errorMessage);
+	public CorruptedScriptException (String errorMessage, int frame, Throwable cause) {
+		super(errorMessage + (frame == -1 ? "" : " \nat frame " + frame), cause);
 
 		Logger.log(errorMessage);
 		String fullErrorMessage = errorMessage + " \nPlease check your script manually " + (frame == -1 ? "" : " \nat frame " + frame);
 		JOptionPane.showMessageDialog(null, fullErrorMessage, "An exception occured", JOptionPane.ERROR_MESSAGE);
+	}
+	public CorruptedScriptException (String errorMessage, int frame) {
+		this(errorMessage, frame, null);
 	}
 }
